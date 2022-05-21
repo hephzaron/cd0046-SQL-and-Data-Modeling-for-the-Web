56a8acdf29b36b012a07cb9af9e135c39b8e361d
Daramola Tobi <tobi_daramola@yahoo.com>
2022-05-21 20:40:23 +0100

    Initialize schema migration for venue and artist

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ from flask_wtf import Form
 from forms import *
 from flask_migrate import Migrate
 from config.base import db
+
+#----------------------------------------------------------------------------#
+# Import Models.
+#----------------------------------------------------------------------------#
+from models.artist import Artist
+from models.venue import Venue
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -24,24 +30,8 @@ db.init_app(app)
 moment = Moment(app)
 # TODO: connect to a local postgresql database
 migrate = Migrate(app, db)
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-class Artist(db.Model):
-    __tablename__ = 'Artist'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
 
-    # TODO: implement any missing fields, as a database migration using Flask-Migrate
-    def __repr__(self):
-        return f'<Artist {self.id} {self.name} {self.state}>'
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 
--- models/artist.py
+++ models/artist.py
@@ -1,17 +1,17 @@
 from config.base import db
 
-class Venue(db.Model):
-    __tablename__ = 'Venue'
+class Artist(db.Model):
+    __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     city = db.Column(db.String(120), nullable=False)
     state = db.Column(db.String(120), nullable=False)
-    address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120), nullable=False)
+    genres = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
     def __repr__(self):
-        return f'<Venue {self.id} {self.name} {self.state}>'
+        return f'<Artist {self.id} {self.name} {self.state}>'
