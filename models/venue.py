0f6b67edd85377cd182087a1de3677fba1a2aa19
Daramola Tobi <tobi_daramola@yahoo.com>
2022-05-22 20:09:08 +0100

    Create migrations for Venue, Show, Artist Model

--- models/venue.py
+++ models/venue.py
@@ -4,13 +4,16 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, nullable=False)
+    name = db.Column(db.String(), nullable=False)
     city = db.Column(db.String(120), nullable=False)
     state = db.Column(db.String(120), nullable=False)
     address = db.Column(db.String(120), nullable=False)
     phone = db.Column(db.String(120), nullable=False)
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
+    
+    # 
+    artist = db.relationship('Artist', secondary='Show', backref=db.backref('venue', lazy=True))
 
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
     def __repr__(self):
