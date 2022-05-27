c767ba3df71a153288989ea7d69edada210df7c8
Daramola Tobi <tobi_daramola@yahoo.com>
2022-05-27 18:44:23 +0100

    feature: set up WTForms with SECRET_KEY

--- app.py
+++ app.py
@@ -37,12 +37,15 @@ DATABASE_PASSWORD=config('DATABASE_PASSWORD')
 DATABASE_SERVER=config('DATABASE_SERVER')
 DATABASE_NAME=config('DATABASE_NAME')
 SQLALCHEMY_TRACK_MODIFICATIONS = config('SQLALCHEMY_TRACK_MODIFICATIONS')
+SECRET_KEY = config('SECRET_KEY')
 
 app.config[
   'SQLALCHEMY_DATABASE_URI'
   ] = f'postgresql://{DATABASE_USERNAME}:{DATABASE_PASSWORD}@{DATABASE_SERVER}/{DATABASE_NAME}'
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = SQLALCHEMY_TRACK_MODIFICATIONS
+
+app.config['SECRET_KEY'] = SECRET_KEY
 
 db.init_app(app)
 
@@ -135,88 +138,47 @@ def search_venues():
 def show_venue(venue_id):
   # shows the venue page with the given venue_id
   # TODO: replace with real venue data from the venues table, using venue_id
-  venue = db.session.query(Venue).filter(Venue.id==venue_id).options(
-    joinedload(Venue.genres).options(load_only(Genre.name))).all()
-  print('venue', venue)
-  data1={
-    "id": 1,
-    "name": "The Musical Hop",
-    "genres": ["Jazz", "Reggae", "Swing", "Classical", "Folk"],
-    "address": "1015 Folsom Street",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "123-123-1234",
-    "website": "https://www.themusicalhop.com",
-    "facebook_link": "https://www.facebook.com/TheMusicalHop",
-    "seeking_talent": True,
-    "seeking_description": "We are on the lookout for a local artist to play every two weeks. Please call us.",
-    "image_link": "https://images.unsplash.com/photo-1543900694-133f37abaaa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60",
-    "past_shows": [{
-      "artist_id": 4,
-      "artist_name": "Guns N Petals",
-      "artist_image_link": "https://images.unsplash.com/photo-1549213783-8284d0336c4f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=80",
-      "start_time": "2019-05-21T21:30:00.000Z"
-    }],
-    "upcoming_shows": [],
-    "past_shows_count": 1,
-    "upcoming_shows_count": 0,
-  }
-  data2={
-    "id": 2,
-    "name": "The Dueling Pianos Bar",
-    "genres": ["Classical", "R&B", "Hip-Hop"],
-    "address": "335 Delancey Street",
-    "city": "New York",
-    "state": "NY",
-    "phone": "914-003-1132",
-    "website": "https://www.theduelingpianos.com",
-    "facebook_link": "https://www.facebook.com/theduelingpianos",
-    "seeking_talent": False,
-    "image_link": "https://images.unsplash.com/photo-1497032205916-ac775f0649ae?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-    "past_shows": [],
-    "upcoming_shows": [],
-    "past_shows_count": 0,
-    "upcoming_shows_count": 0,
-  }
-  data3={
-    "id": 3,
-    "name": "Park Square Live Music & Coffee",
-    "genres": ["Rock n Roll", "Jazz", "Classical", "Folk"],
-    "address": "34 Whiskey Moore Ave",
-    "city": "San Francisco",
-    "state": "CA",
-    "phone": "415-000-1234",
-    "website": "https://www.parksquarelivemusicandcoffee.com",
-    "facebook_link": "https://www.facebook.com/ParkSquareLiveMusicAndCoffee",
-    "seeking_talent": False,
-    "image_link": "https://images.unsplash.com/photo-1485686531765-ba63b07845a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=747&q=80",
-    "past_shows": [{
-      "artist_id": 5,
-      "artist_name": "Matt Quevedo",
-      "artist_image_link": "https://images.unsplash.com/photo-1495223153807-b916f75de8c5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80",
-      "start_time": "2019-06-15T23:00:00.000Z"
-    }],
-    "upcoming_shows": [{
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-01T20:00:00.000Z"
-    }, {
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-08T20:00:00.000Z"
-    }, {
-      "artist_id": 6,
-      "artist_name": "The Wild Sax Band",
-      "artist_image_link": "https://images.unsplash.com/photo-1558369981-f9ca78462e61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=794&q=80",
-      "start_time": "2035-04-15T20:00:00.000Z"
-    }],
-    "past_shows_count": 1,
-    "upcoming_shows_count": 1,
-  }
-  data = list(filter(lambda d: d['id'] == venue_id, [data1, data2, data3]))[0]
-  return render_template('pages/show_venue.html', venue=data)
+  # Get the queries of all required columns
+  all_columns = select(Venue).where(Venue.id==venue_id).subquery()
+  
+  # Get the genre list of requested venue id
+  genre_list = db.session.query(
+    MusicGenre.venue_id, Genre.name
+    ).join(MusicGenre).filter(MusicGenre.venue_id==venue_id).all()
+  
+  venue = db.session.query(all_columns).first()
+  
+  # Convert into a readable dict format
+  venue = dict(venue)
+  
+  # Get a list of past shows in a venue by various artist
+  past_shows = db.session.query(
+    Artist.id.label('artist_id'),
+    Artist.name.label('artist_name'),
+    Artist.image_link.label('artist_image_link'),
+    Show.start_time).outerjoin(Show).filter(
+    Show.venue_id==venue_id, func.date(Show.start_time)<date.today()).all()
+    
+    
+  
+  # Get a list of upcoming shows in a venue by various artist
+  upcoming_shows = db.session.query(
+    Artist.id.label('artist_id'),
+    Artist.name.label('artist_name'),
+    Artist.image_link.label('artist_image_link'),
+    Show.start_time).outerjoin(Show).filter(
+    Show.venue_id==venue_id, func.date(Show.start_time)>=date.today()).all()
+    
+  past_shows_count = len(past_shows)
+  upcoming_shows_count = len(upcoming_shows)
+    
+  venue['genres'] = list(genre_list)
+  venue['past_shows'] = list(past_shows)
+  venue['upcoming_shows'] = list(upcoming_shows)
+  venue['past_shows_count'] = past_shows_count
+  venue['upcoming_shows_count'] = upcoming_shows_count
+          
+  return render_template('pages/show_venue.html', venue=venue)
 
 #  Create Venue
 #  ----------------------------------------------------------------
--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 import os
+import secrets
+
 SECRET_KEY = os.urandom(32)
 # Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -14,3 +16,6 @@ SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:postgres@localhost:5432/fyyur'
 
 # TRACK MODIFICATIONS
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# SECRET KEY FOR WTFORM
+SECRET_KEY = secrets.token_urlsafe()
