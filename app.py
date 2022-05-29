b5392b339898151787c94e5beaac8d6483a1e3b4
Daramola Tobi <tobi_daramola@yahoo.com>
2022-05-29 19:45:20 +0100

    refactor: parse date in readable format

--- app.py
+++ app.py
@@ -59,12 +59,12 @@ migrate = Migrate(app, db)
 #----------------------------------------------------------------------------#
 
 def format_datetime(value, format='medium'):
-  date = dateutil.parser.parse(value)
-  if format == 'full':
-      format="EEEE MMMM, d, y 'at' h:mma"
-  elif format == 'medium':
-      format="EE MM, dd, y h:mma"
-  return babel.dates.format_datetime(date, format, locale='en')
+      date = dateutil.parser.parse(value) if isinstance(value, str) else value
+      if format == 'full':
+            format="EEEE MMMM, d, y 'at' h:mma"
+      elif format == 'medium':
+            format="EE MM, dd, y h:mma"
+      return babel.dates.format_datetime(date, format, locale='en')
 
 app.jinja_env.filters['datetime'] = format_datetime
 
