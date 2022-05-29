5d2799599a8f34d9867850c8599b9bf0fd15df98
Daramola Tobi <tobi_daramola@yahoo.com>
2022-05-29 13:57:25 +0100

    feature: user can create a show

--- app.py
+++ app.py
@@ -411,7 +411,7 @@ def create_artist_submission():
     db.session.add(artist)
     db.session.commit()
     flash('Artist ' +request.form['name']  + ' was successfully listed!')
-  except:
+  except SQLAlchemyError as e:
     db.session.rollback()
     # TODO: on unsuccessful db insert, flash an error instead.
     flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.', 'error')
@@ -430,7 +430,7 @@ def shows():
   # TODO: replace with real venues data.
   shows = db.session.query(
     Show.venue_id,
-    Venue.name,
+    Venue.name.label('venue_name'),
     Show.artist_id,
     Artist.name.label('artist_name'),
     Artist.image_link.label('artist_image_link'),
@@ -449,9 +449,22 @@ def create_shows():
 def create_show_submission():
   # called to create new shows in the db, upon submitting new show listing form
   # TODO: insert form data as a new Show record in the db, instead
-
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
+  form = ShowForm(formdata=MultiDict(request.form))
+  formdata = form.data
+  # Remove csrf_token from data to be persisted to db
+  formdata.pop('csrf_token')
+ 
+  show = Show(**formdata)
+  try:
+    db.session.add(show)
+    db.session.commit()
+    flash('Show was successfully listed!')
+  except SQLAlchemyError as e:
+    db.session.rollback()
+    # TODO: on unsuccessful db insert, flash an error instead.
+    flash('An error occurred. Show could not be listed.')
+  finally:
+    db.session.close()
   # TODO: on unsuccessful db insert, flash an error instead.
   # e.g., flash('An error occurred. Show could not be listed.')
   # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
