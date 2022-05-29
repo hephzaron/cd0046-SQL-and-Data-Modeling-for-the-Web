8ec72152c20e1d5fc223592cfb91808623afc00f
Daramola Tobi <tobi_daramola@yahoo.com>
2022-05-29 05:53:44 +0100

    feature: user can create a new artist

--- app.py
+++ app.py
@@ -401,10 +401,22 @@ def create_artist_submission():
   # called upon submitting the new artist listing form
   # TODO: insert form data as a new Venue record in the db, instead
   # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Artist ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
+  form = ArtistForm(formdata=MultiDict(request.form))
+  formdata = form.data
+  # Remove csrf_token from data to be persisted to db
+  formdata.pop('csrf_token')
+ 
+  artist = Artist(**formdata)
+  try:
+    db.session.add(artist)
+    db.session.commit()
+    flash('Artist ' +request.form['name']  + ' was successfully listed!')
+  except:
+    db.session.rollback()
+    # TODO: on unsuccessful db insert, flash an error instead.
+    flash('An error occurred. Artist ' + request.form['name'] + ' could not be listed.', 'error')
+  finally:
+    db.session.close()
   # e.g., flash('An error occurred. Artist ' + data.name + ' could not be listed.')
   return render_template('pages/home.html')
 
